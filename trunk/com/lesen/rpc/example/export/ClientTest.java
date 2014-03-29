391378bf628a62d5e89129ce2acc1fbd1da9f67f
980127462@qq.com <980127462@qq.com@37b557f0-2252-f0b4-a451-91a3eb7ab949>
2014-03-29 15:52:10 +0000

    import source

---
+++ trunk/com/lesen/rpc/example/export/ClientTest.java
@@ -0,0 +1,19 @@
+package com.lesen.rpc.example;
+
+import com.lesen.rpc.client.RPCClient;
+import com.lesen.rpc.common.export.Service;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		String serverName = "test";
+		String rpcUri = "rpc://127.0.0.1:1082";
+		RPCClient client = new RPCClient(rpcUri);
+		client.registDecodeEncode(new PersonDecodeEncode());
+		client.connectService();
+		
+		Service service = client.getRemoteService(serverName, Service.class);
+		System.out.println(service.test("12"));
+		client.close();
+	}
+}
